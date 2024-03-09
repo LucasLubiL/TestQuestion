bbdcf4815c218dc9622e844944dfaef6e8f9355d
Lucas Amaral <144184205+LucasLubiL@users.noreply.github.com>
2024-03-09 14:10:31 -0300

    Add files via upload

---
+++ Inverter.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    char palavra[11]="Programacao";
+
+    for(int i=10;i>=0;i--){
+        printf("%c",palavra[i]);
+
+    }
+
+    return 0;
+}
